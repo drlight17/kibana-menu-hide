4685d807f865ff7cdce0323763017a9e64ecffca
drlight17 <37434652+drlight17@users.noreply.github.com>
2020-07-15 11:09:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export default function(kibana) {
   return new kibana.Plugin({
     uiExports: {
       hacks: [
-        'plugins/KSC/app'
+        'plugins/kibana-menu-hide/app'
       ],
 //      ]
       styleSheetPaths: require('path').resolve(__dirname, 'public/app.css')
