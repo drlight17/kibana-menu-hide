a9ddec9587950d828381c52b828851c9996c1a4d
drlight17 <37434652+drlight17@users.noreply.github.com>
2020-07-15 11:05:57 +0300

    fix of all known bugs
    fixed hiding and icons in dark mode

--- public/app.js
+++ public/app.js
@@ -1,18 +1,21 @@
 import $ from 'jquery';
 
 $(document).ready(function($) {
-    $('.homWelcome').hide();
-    // 2 methods below to remove href from kibana logo is not working now so commented out
-//    $('a[href*="/app/kibana#/home"]').removeAttr("href");
-//    $('.euiHeaderLogo').removeAttr("href");
+    // fix of icons in dark mode
+    if ($('link[href*=".dark.css"]').attr('href')) {
+        $('.euiImage__img').css("filter","invert(1)");
+    }
+    // add timeout hack to hide unnecessaary elements
+    function hideMenuElements () {
+        $('.euiHeaderLogo').removeAttr("href");
     // if shere is no security-configuration menu item on the page - hide some menu elements
-//    if (!$('a[href*="/security-configuration#/"]').attr('href')) {
-    if (!$('a[data-name*="Security"]').length) {
-        $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(4)').hide();
-        $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(5)').hide();
-        $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(6)').hide();
-        $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(7)').hide();
-        $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(8)').hide();
-        $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(9)').hide();
-    };
+        if (!$('a[data-name*="Security"]').length) {
+            $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(4)').hide();
+            $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(5)').hide();
+            $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(6)').hide();
+            $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(7)').hide();
+            $('#navDrawerMenu > ul:nth-child(4) > li:nth-child(8)').hide();
+        };
+    }
+    setTimeout (hideMenuElements, 100);
 });
